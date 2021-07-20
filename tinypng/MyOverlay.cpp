3abe1f90d35ad4462d390431193d11f96a96cfbd
Yong++ <yongplus@foxmail.com>
2021-07-20 16:51:43 +0800

    优化

--- tinypng/MyOverlay.cpp
+++ tinypng/MyOverlay.cpp
@@ -13,12 +13,21 @@ MyOverlay::MyOverlay(QWidget* parent)
 	QLabel* label = new QLabel();
 	label->setObjectName("overlaylabel");
 
-	label->setStyleSheet("#overlaylabel{color:#FFFFFF;background-color:rgba(0,0,0,0);}");
+	label->setStyleSheet("#overlaylabel{color:#FFFFFF;background-color:rgba(0,0,0,0);letter-spacing:10px;}");
 	label->setAttribute(Qt::WA_TransparentForMouseEvents, true);
 	label->setText("松开读取目录资源");
 
+	QImage* img = new QImage();
+	img->load(":/res/read.png");
+
+
+	QLabel* label2 = new QLabel();
+	label2->setPixmap(QPixmap::fromImage(*img));
 	QVBoxLayout* layout = new QVBoxLayout();
-	layout->addWidget(label, 1, Qt::AlignHCenter);
+	layout->addStretch(1);
+	layout->addWidget(label2, 0, Qt::AlignHCenter);
+	layout->addWidget(label, 0, Qt::AlignHCenter);
+	layout->addStretch(1);
 	setAttribute(Qt::WA_TransparentForMouseEvents, true);
 	setLayout(layout);
 	hide();
